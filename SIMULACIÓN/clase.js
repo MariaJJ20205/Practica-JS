a260ee96bed9810c038f87eaa524adb40930d3c8
MariaJJ20205 <161511235+MariaJJ20205@users.noreply.github.com>
2024-09-04 12:47:15 -0500

    Add files via upload

---
+++ SIMULACIÓN/clase.js
@@ -0,0 +1,9 @@
+let nombre = "Maria";
+console.log(nombre);
+
+const usuario = "Maribbe";
+// usuario = 33;
+console.log(usuario)
+
+let mensaje ='El nombre es ' +nombre+ ' y el usuario es ' + usuario
+console.log(mensaje)
